2024e6915d844ced0435cf0bee39fbf8e20eadb0
StelianDimitrov <Brglz@users.noreply.github.com>
2020-12-16 20:51:20 +0200

    feat/dynamic load bio info and edit

--- src/app/about/about-edit/about-edit.component.ts
+++ src/app/about/about-edit/about-edit.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutService } from '../about.service';
 
 @Component({
   selector: 'app-about-edit',
   templateUrl: './about-edit.component.html',
-  styleUrls: ['./about-edit.component.scss']
+  styleUrls: ['./about-edit.component.scss'],
+  providers: [AboutService]
 })
 export class AboutEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private aboutService: AboutService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(data) {
+    return this.aboutService.editAbout(data).subscribe()
   }
 
 }
---
+++ src/app/about/about-interface.service.ts
@@ -0,0 +1,6 @@
+export interface AboutInterface {
+  name: string,
+  imgURL: string,
+  description: string,
+  objectId: string
+}
